2e0d069e2a7e8851dc00930c9d0fc025b585f7ca
Mariia Kasianova <kasyanovamg@gmail.com>
2018-01-19 01:24:21 +0400

    feat: completed css styles, added "add book" button interaction

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 $(document).ready(function(){
 
-var open_book_edit = $("#edition_field");
-
 //toggles add/edit form
-function openBook() { 
-	$("#add_book").click(function(){
-        open_book_edit.toggle(500);
-    });
+var open_book_edit = $("#edition_field");
+$("#add_book").click(function(){
+    open_book_edit.toggle(400);
+    clear();
+});
+
+//clears all inputs
+function clear () {
+	$("#book_name").val("");
+	$("#author_name").val("");
+	$("#year").val("");
+	$("#pic").val("");
 }
 
+//listens to event of cancel button
+$("#cancel_book").click(function() {
+	clear();
+	open_book_edit.hide(400);
+});
 
 
-openBook(); 
+
+
 
 });
